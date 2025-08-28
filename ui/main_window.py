3a64a3c3538a46dbbbbcfd529d52b768a9e4b6a0
Andres Trujillo Sosa <202360022@ucc.mx>
2025-08-28 10:28:12 -0600

    New Main Layout

--- ui/main_window.py
+++ ui/main_window.py
@@ -10,10 +10,13 @@ from PySide6.QtWidgets import (
     QFileDialog,
     QPushButton,
 )
-from PySide6.QtCore import Qt, QUrl
-from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
+from PySide6.QtCore import Qt, QUrl, QByteArray
+from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput, QMediaMetaData
+from PySide6.QtGui import QPixmap, QImage
 import json
 from pathlib import Path
+
+
 
 
 class MainWindow(QMainWindow):
@@ -22,56 +25,82 @@ class MainWindow(QMainWindow):
 
         WINDOW_X = 100
         WINDOW_Y = 100
-        WINDOW_WIDTH = 400
+        WINDOW_WIDTH = 600
         WINDOW_HEIGHT = 800
         self.media_player = QMediaPlayer()
         self.music_files_paths = []
         self.audio_output = QAudioOutput()
         self.song_progress = QSlider(Qt.Orientation.Horizontal)
-        self.music_list = QListWidget()
-        self.stop_button = QPushButton("Stop Music")
-        self.song_name = QLabel()
+        self.music_list = QListWidget() # Lista de canciones disponibles
+        self.toggle_media_button = QPushButton("Stop Music") # boton de pausa/reproducir
+        self.song_name = QLabel(wordWrap=True, text="Start playing some music...")
         self.time_passed = QLabel(text="0:00")
         self.time_total = QLabel(text="0:00")
+        self.album_cover = QLabel()
         self.select_directory_button = None  # Añadimos una referencia al botón
-        central_widget = QWidget()  # Widget vacio sobre el cual distribuir la ventana
-        main_layout = QVBoxLayout()  # Layout vertical para manejar el widget central
-        inner_info_layout = QVBoxLayout()  # Layout vertical para manejar el nombre de la cancion
-        outer_info_layout = QHBoxLayout()  # QH PARA LA BARRA DE PROGRESO y la duracion
+        central_widget = QWidget()  # Widget vacio que actua como base de la ventana
+        lower_info_layout = QHBoxLayout()  # QH PARA LA BARRA DE PROGRESO y la duracion, se une a upper info.
+        app_layout = QHBoxLayout() #Contendra dos sub-layouts
+        sidebar_layout = QVBoxLayout() # controles de reproduccion, volumen, Datos, Queue
+        content_layout = QVBoxLayout() # Este layout contendra todo lo que estaba en main_layout
         config_file = Path("config.json")
 
         self.setWindowTitle("Mellomaniac — Music Player")  # Music Player string should change for the current song's name
         self.setGeometry(WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT)
 
-        central_widget.setLayout(main_layout)
+        central_widget.setLayout(app_layout)
         self.setCentralWidget(central_widget)
-
         self.song_name.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        inner_info_layout.addWidget(self.song_name)
-        outer_info_layout.addWidget(self.time_passed)
-        outer_info_layout.addWidget(self.song_progress)
-        outer_info_layout.addWidget(self.time_total)
-        inner_info_layout.addLayout(outer_info_layout)
-        main_layout.addLayout(inner_info_layout)
-        main_layout.addWidget(self.music_list)
-        main_layout.addWidget(self.stop_button)
+        lower_info_layout.addWidget(self.time_passed)
+        lower_info_layout.addWidget(self.song_progress)
+        lower_info_layout.addWidget(self.time_total)
+        content_layout.addLayout(lower_info_layout)
+        content_layout.addWidget(self.music_list)
+        sidebar_layout.addWidget(self.album_cover)
+        sidebar_layout.addWidget(self.song_name)
+        sidebar_layout.addWidget(self.toggle_media_button)
+        app_layout.addLayout(sidebar_layout)
+        app_layout.addLayout(content_layout)
 
         if not config_file.exists():
             self.select_directory_button = QPushButton("Selecciona tu directorio de musica...")
-            main_layout.addWidget(self.select_directory_button)
+            content_layout.addWidget(self.select_directory_button)
             self.select_directory_button.clicked.connect(self.select_music_directory)
-
         else:
             music_path = self.get_music_path()
             self.load_music(music_path)
 
         self.music_list.itemClicked.connect(self.play_song)
-        self.stop_button.clicked.connect(self.toggle_play_pause)
+        self.toggle_media_button.clicked.connect(self.toggle_play_pause)
         self.media_player.playbackStateChanged.connect(self.update_play_button)
         self.media_player.durationChanged.connect(self.update_duration)
         self.media_player.positionChanged.connect(self.update_progress)
+        self.media_player.mediaStatusChanged.connect(self.update_metadata)
         self.song_progress.sliderMoved.connect(self.change_position)
+        
+        #Fin de __init__()
 
+
+    def update_metadata(self, status: QMediaPlayer.MediaStatus) -> None:
+        if status == QMediaPlayer.MediaStatus.LoadedMedia:
+            # Obtener el objeto de metadatos
+            metadata = self.media_player.metaData()
+            self.song_name.setText(metadata.value(QMediaMetaData.Key.Title))
+            # Intentar obtener la imagen directamente
+            cover_art_image = metadata.value(QMediaMetaData.Key.CoverArtImage)
+            
+            if cover_art_image and isinstance(cover_art_image, QImage):
+                pixmap = QPixmap.fromImage(cover_art_image)
+                self.album_cover.setPixmap(
+                    pixmap.scaled(
+                        self.album_cover.size(),
+                        aspectMode=Qt.AspectRatioMode.KeepAspectRatio,
+                        mode=Qt.TransformationMode.SmoothTransformation
+                    )
+                )
+                print("Portada de álbum encontrada y mostrada.")
+                return
+            
     def select_music_directory(self) -> None: # Slot para seleccionar el directorio de musica
         music_directory = QFileDialog.getExistingDirectory()
         if music_directory:
@@ -95,7 +124,6 @@ class MainWindow(QMainWindow):
     def play_song(self, song: QListWidgetItem):
         song_path = self.music_files_paths[self.music_list.row(song)]
         song_url = QUrl.fromLocalFile(str(song_path))
-        self.song_name.setText(song.text())
         self.media_player.setSource(song_url)
         self.media_player.setAudioOutput(self.audio_output)
         self.media_player.play()
@@ -109,18 +137,17 @@ class MainWindow(QMainWindow):
     def toggle_play_pause(self) -> None:
         if self.media_player.isPlaying():
             self.media_player.pause()
-
         else:
             self.media_player.play()
 
     def update_play_button(self, playback_state: QMediaPlayer.PlaybackState) -> None:
         if playback_state == QMediaPlayer.PlaybackState.PlayingState:
-            self.stop_button.setText("Pause Music")
+            self.toggle_media_button.setText("Pause Music")
         elif (
             playback_state == QMediaPlayer.PlaybackState.PausedState
             or playback_state == QMediaPlayer.PlaybackState.StoppedState
         ):
-            self.stop_button.setText("Play Music")
+            self.toggle_media_button.setText("Play Music")
 
     def update_duration(self, duration: int) -> None:
         self.song_progress.setMaximum(duration)
@@ -138,3 +165,6 @@ class MainWindow(QMainWindow):
 
     def change_position(self, position: int) -> None:
         self.media_player.setPosition(position)
+
+
+#TODO: Load song cover from metadata
